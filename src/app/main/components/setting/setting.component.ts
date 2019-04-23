b7b297de338c1eca9f1635204f98ff16b2e3979d
59160548 <59160548@go.buu.ac.th>
2019-04-24 00:06:47 +0700

    implement systeminterface to setting components

--- src/app/main/components/setting/setting.component.ts
+++ src/app/main/components/setting/setting.component.ts
@@ -1,23 +1,46 @@
 import { Component, OnInit } from '@angular/core';
+import { SystemInterface, System } from 'src/app/service/interface/system';
+import { SystemService } from 'src/app/service/system.service';
+import { AlertService } from 'src/app/service/alert.service';
 
 @Component({
   selector: 'app-setting',
   templateUrl: './setting.component.html',
   styleUrls: ['./setting.component.sass']
 })
-export class SettingComponent implements OnInit {
+export class SettingComponent implements OnInit, SystemInterface {
+
+  systemInfo: System;
+
   bsValue = new Date();
   term1: Date[];
   term2: Date[];
   maxDate = new Date();
 
-  constructor() {    
+  constructor(private service: SystemService,
+    private alert: AlertService) {
     this.maxDate.setDate(this.maxDate.getDate() + 7);
     this.term1 = [this.bsValue, this.maxDate];
     this.term2 = [this.bsValue, this.maxDate];
   }
 
   ngOnInit() {
+    this.maxDate.setDate(this.maxDate.getDate() + 7);
+    this.term1 = [this.bsValue, this.maxDate];
+    this.term2 = [this.bsValue, this.maxDate];
+  }
+
+  onupdate(): void {
+    this.service.insertSystemData({
+      courseYear: 1,
+      courseTerm1: "1",
+      courseTerm2: "1"
+    }).then((result) => {
+      this.alert.notify(result.message);
+      this.ngOnInit();
+    }).catch((err) => {
+      this.alert.notify(err.message);
+    });
   }
 
 }
