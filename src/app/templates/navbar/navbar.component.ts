be33598c3b77e01a9fa6cf0dc42380e58795dce1
59160548 <59160548@go.buu.ac.th>
2019-05-01 13:17:35 +0700

    fixed navbar undone

--- src/app/templates/navbar/navbar.component.ts
+++ src/app/templates/navbar/navbar.component.ts
@@ -41,4 +41,10 @@ export class NavbarComponent implements OnInit {
     });
   }
 
+  ngDoCheck(): void {
+    //Called every time that the input properties of a component or a directive are checked. Use it to extend change detection by performing a custom check.
+    //Add 'implements DoCheck' to the class.
+    this.systemData = this.system.systemData
+  }
+
 }
