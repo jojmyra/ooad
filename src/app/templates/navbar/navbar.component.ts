cb8fe98270babacdbf2de31b03a28ebab2d1f3c7
59160548 <59160548@go.buu.ac.th>
2019-05-01 13:08:39 +0700

    fixed load complicate in navbar

--- src/app/templates/navbar/navbar.component.ts
+++ src/app/templates/navbar/navbar.component.ts
@@ -4,7 +4,7 @@ import { AlertService } from 'src/app/service/alert.service';
 import { AuthenticatorService } from 'src/app/authenticator.service';
 import { SystemService } from 'src/app/service/system.service';
 import { System } from 'src/app/service/interface/system';
-
+declare let App;
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
@@ -18,12 +18,7 @@ export class NavbarComponent implements OnInit {
     private alert: AlertService,
     private authenticator: AuthenticatorService,
     private system: SystemService) {
-      this.system.getSystemData().then((result) => {
-        this.system.setSystemData(result)
-        this.systemData = this.system.systemData
-      }).catch(() => {
-        this.alert.notify('เรียกข้อมูลจากตั้งค่าระบบไม่ได้', 'info')
-      });
+
   }
 
   ngOnInit() {
@@ -33,6 +28,17 @@ export class NavbarComponent implements OnInit {
     this.alert.notify('ออกจากระบบสำเร็จ', 'info')
     this.authenticator.clearAuthenticated();
     this.router.navigate(['/'])
+  }
+
+  private initialSystemData() {
+    this.systemData = this.system.systemData
+    if (this.systemData) return setTimeout(() => App.initialLoadPage(), 100)
+    this.system.getSystemData().then((result) => {
+      this.system.setSystemData(result)
+      this.systemData = this.system.systemData
+    }).catch(() => {
+      this.alert.notify('เรียกข้อมูลจากตั้งค่าระบบไม่ได้', 'info')
+    });
   }
 
 }
