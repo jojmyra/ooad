c1e71d7958b18d4606aa15631837980a6471be37
59160548 <59160548@go.buu.ac.th>
2019-05-14 12:33:10 +0700

    delete building implement done

--- src/app/main/components/manager-building/delete-building/delete-building.component.ts
+++ src/app/main/components/manager-building/delete-building/delete-building.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit, Input, TemplateRef } from '@angular/core';
 import { BsModalService, BsModalRef } from 'ngx-bootstrap/modal';
+import { BuildingService } from 'src/app/service/building.service';
+import { AlertService } from 'src/app/service/alert.service';
 @Component({
   selector: 'app-delete-building',
   templateUrl: './delete-building.component.html',
@@ -7,9 +9,14 @@ import { BsModalService, BsModalRef } from 'ngx-bootstrap/modal';
 })
 export class DeleteBuildingComponent implements OnInit {
   @Input()
+  _id: any;
+
   modalRef: BsModalRef;
-  message: string;
-  constructor(private modalService: BsModalService) { }
+  message: String;
+  
+  constructor(private modalService: BsModalService,
+    private service: BuildingService,
+    private alert: AlertService) { }
 
   ngOnInit() {
   }
@@ -18,12 +25,23 @@ export class DeleteBuildingComponent implements OnInit {
   }
  
   confirm(): void {
-    this.message = 'Confirmed!';
+    this.service.deleteBuilding(this._id).then((result) => {      
+      this.alert.notify(result.message, 'info')
+      this.service.getBuildings({
+        startPage: 1,
+        limitPage: 5
+      }).then((list) => {
+        this.service.setItemList(list)
+      }).catch((err) => {
+        this.alert.notify(err.message)
+      });
+    }).catch((err) => {
+      this.alert.notify(err.Message)
+    });
     this.modalRef.hide();
   }
  
   decline(): void {
-    this.message = 'Declined!';
     this.modalRef.hide();
   }
 
