4af869935bbb4946d92cc99c4ebe8411d5a31f79
Patdanai Changlao <jojmyra@gmail.com>
2019-05-17 03:47:27 +0700

    fixed bug

--- src/app/main/components/manager-course/add-course/add-course.component.ts
+++ src/app/main/components/manager-course/add-course/add-course.component.ts
@@ -96,6 +96,7 @@ export class AddCourseComponent implements OnInit {
     }).catch((err) => {
       this.alert.someting_wrong()
     });
+    this.professor = new Array();
     this.modalRef.hide();
   }
 
